0b1a43ddfda76a4581ec9a346486dedd802b98b2
itlubber <1830611168@qq.com>
2021-11-08 17:56:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup
 
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 
 setup(
     name='packaging_tutorial',
     version='0.0.1',
     packages=setuptools.find_packages(),
-    url='https://github.com/itlubber/sampleproject',
+    url='https://github.com/itlubber/itlubber',
     license='MIT',
     author='itlubber',
     author_email='1830611168@qq.com',
     description='public methods for itlubber',
     long_description=long_description,
     long_description_content_type="itlubber/methods",
-    install_requires=['time', 'numpy', 'matplotlib', 'sklearn'],
+    install_requires=['time', 'numpy', 'pandas', 'matplotlib', 'sklearn'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
